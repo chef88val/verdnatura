fcd313c150088d410541a241fcc9091bd386f1b7
Javier Segarra Martínez <jsegarra@iti.es>
2023-07-11 22:33:51 +0200

    define controller to handle each endpoint

---
+++ modules/book/controller.js
@@ -0,0 +1,45 @@
+const { logger } = require("../../logger")
+const Book = require("./book")
+
+const get = ((req, res) => {
+    Book.find({})
+        .then(result => res.status(200).json({ result }))
+        .catch(error => res.status(500).json({ msg: error }))
+})
+
+
+const getById = ((req, res) => {
+    Book.findOne({ _id: req.params.bookID })
+        .then(result => res.status(200).json({ result }))
+        .catch(() => res.status(404).json({ msg: 'Book not found' }))
+})
+
+const post = ((req, res) => {
+    Book.create(req.body)
+        .then(result => res.status(200).json({ result }))
+        .catch((error) => res.status(500).json({ msg: error }))
+})
+
+const put = ((req, res) => {
+    Book.findOneAndUpdate({ _id: req.params.bookID }, req.body, { new: true, runValidators: true })
+        .then(result => res.status(200).json({ result }))
+        .catch((error) => res.status(404).json({ msg: 'Book not found' }))
+
+})
+
+
+const remove = ((req, res) => {
+    Book.findOneAndDelete({ _id: req.params.bookID })
+        .then(result => res.status(200).json({ result }))
+        .catch((error) => res.status(404).json({ msg: 'Book not found' }))
+
+})
+
+module.exports = {
+    get,
+    getById,
+    post,
+    put,
+    remove
+}
+
